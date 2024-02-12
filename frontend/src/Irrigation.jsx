524989fd71441b3d41a51c6c76da1b5d1a00c55b
quentin <quentinkamwendo673@gmail.com>
2024-02-13 00:56:53 +0200

    interactive navbar

--- frontend/src/Irrigation.jsx
+++ frontend/src/Irrigation.jsx
@@ -1,12 +1,33 @@
-import { Button } from '@chakra-ui/react';
-import React from 'react';
+import { LockIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
 
 const Irrigation = () => {
-    return (
-        <div className='bg-violet-100'>
-            <Button colorScheme='green' className='m-4'>Irrigate</Button>
-        </div>
-    );
-}
+  return (
+    <div className="bg-violet-100">
+      <Card m={2} mt={2}>
+        <CardHeader>
+          <Heading>IoT data</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>soil moisture is: 0</Text>
+          <Text>Current temperature is: 0</Text>
+          <Text>Current river flow is: 0</Text>
+        </CardBody>
+      </Card>
+      <Button colorScheme="green" className="m-4">
+        <LockIcon mr={2} />
+        Irrigate
+      </Button>
+    </div>
+  );
+};
 
 export default Irrigation;
--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import {
+  Box,
+  Collapse,
+  Flex,
+  IconButton,
+  Spacer,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 
 const NavBar = () => {
-    return (
-        <div>
-            <nav className='flex justify-between bg-indigo-400'>
-                <Link to={'/home'} className='hover:bg-lime-300 p-3'>Home</Link>
-                <Link to={'/irrigation'} className='hover:bg-lime-300 p-3'>Monitor Irrigation</Link>
-            </nav>
-            <Outlet />
-        </div>
-    );
-}
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <div>
+      <Flex
+        as={"nav"}
+        align={"center"}
+        justify={"space-between"}
+        bg={"indigo"}
+        color={"white"}
+      >
+        <Box p={4}>
+          <IconButton
+            display={{ base: "block", md: "none" }}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            onClick={onToggle}
+            aria-label={isOpen ? "Close menu" : "Open menu"}
+          />
+        </Box>
+        <Spacer />
+        <Box display={{ base: "none", md: "flex" }} alignItems={'center'}>
+          <>
+            <Link className="hover:bg-violet-400 p-2" to={"/home"}>
+              <Text mr={4} cursor={"pointer"}>
+                Home
+              </Text>
+            </Link>
+            <Link className="hover:bg-violet-400 p-2" to={"/irrigation"}>
+              <Text mr={4} cursor={"pointer"}>
+                Irrigate
+              </Text>
+            </Link>
+          </>
+        </Box>
+        <Collapse in={isOpen} animateOpacity>
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            alignItems={"center"}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: "auto" }}
+          >
+            <Link className="p-2" to={"/home"}>Home</Link>
+            <Link className="p-2" to={"/irrigation"}>Irrigate</Link>
+          </Flex>
+        </Collapse>
+      </Flex>
+      <Outlet />
+    </div>
+  );
+};
 
 export default NavBar;
